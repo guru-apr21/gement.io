d6e4f02b108ac60315a6895ba13756fdea70c8ba
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-12-02 22:28:13 +0530

    Join room, emit message

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
+
 const PORT = process.env.PORT || 5000;
 
 const router = require('./router');
@@ -16,15 +18,23 @@ const io = socketio(server, {
 });
 
 io.on('connection', (socket) => {
-  console.log('We have a new Connection!!!');
-
   socket.on('join', ({ name, room }, callback) => {
-    console.log(name, room);
+    const { error, user } = addUser({ id: socket.id, name, room });
 
-    const error = true;
-    // if (error) {
-    //   callback({ error: 'error' });
-    // }
+    if (error) return callback(error);
+
+    socket.emit('message', {
+      user: 'admin',
+      text: `${user.name}, welcome to the ${user.room}`,
+    });
+
+    socket.broadcast
+      .to(user.room)
+      .emit('message', { user: 'admin', text: `${user.name}, has joined!` });
+
+    socket.join(user.room);
+
+    callback();
   });
 
   socket.on('disconnect', () => {
