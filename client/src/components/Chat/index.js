0dfc3b8cccbeee4a22205d3a25f6d273e5f127d7
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-12-02 22:53:07 +0530

    Add input to emit "sendMessage" event
    Define useEffect to listen to message events and setMessages array

--- client/src/components/Chat/index.js
+++ client/src/components/Chat/index.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
+import './chat.css';
 
 let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
   const ENDPOINT = 'localhost:5000';
 
   useEffect(() => {
@@ -23,7 +26,30 @@ const Chat = ({ location }) => {
     };
   }, [location.search, ENDPOINT]);
 
-  return <div>Chat</div>;
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessage((messages) => setMessages([...messages, message]));
+    });
+  }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message) {
+      socket.emit('sendMessage', message, () => setMessage(''));
+    }
+  };
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyPress={(e) => (e.key === 'Enter' ? sendMessage(e) : null)}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
