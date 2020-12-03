c8a82fac8d77b87f7716a8a13d18b734bf149b59
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-12-03 13:04:06 +0530

    Use ref to get input value 🏄

--- client/src/components/Input/index.js
+++ client/src/components/Input/index.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './input.css';
 
 const Input = ({ socket }) => {
+  const inputRef = useRef();
   const sendMessage = (e) => {
     e.preventDefault();
-    const message = e.target.value;
+    const message = inputRef.current.value;
     if (message) {
-      socket.emit('sendMessage', message, () => (e.target.value = ''));
+      socket.emit('sendMessage', message, () => (inputRef.current.value = ''));
     }
   };
 
   return (
     <form className="form">
       <input
+        ref={inputRef}
         className="input"
         type="text"
         placeholder="Type a message..."
         onKeyPress={(e) => (e.key === 'Enter' ? sendMessage(e) : null)}
       />
-      <button className="sendButton" onClick={(e) => sendMessage(e)}>
+      <button className="sendButton" onClick={sendMessage}>
         Send
       </button>
     </form>
