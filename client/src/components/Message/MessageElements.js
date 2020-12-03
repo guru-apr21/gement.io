4908dea10a9773962d15da4bc4c4f21bec27c6e8
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-12-03 19:51:23 +0530

    Create Styled components for Message 🏉

---
+++ client/src/components/Message/MessageElements.js
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const MessageContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 0 5%;
+  margin-top: 3px;
+  ${({ $style }) => $style ?? {}};
+`;
+
+export const MessageBox = styled.div`
+  background: #f3f3f3;
+  border-radius: 20px;
+  padding: 5px 20px;
+  color: white;
+  display: inline-block;
+  max-width: 80%;
+  ${({ $style }) => $style ?? {}};
+`;
+
+export const MessageText = styled.p`
+  width: 100%;
+  letter-spacing: 0;
+  float: left;
+  font-size: 1.1em;
+  word-wrap: break-word;
+  color: #353535;
+  vertical-align: middle;
+  ${({ $style }) => $style ?? {}};
+`;
+
+export const MessageSentBy = styled.p`
+  display: flex;
+  align-items: center;
+  font-family: Helvetica;
+  color: #828282;
+  letter-spacing: 0.3px;
+  padding-left: 10px;
+`;
