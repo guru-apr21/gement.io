82db3f930ba88933e8d192e2fcdd5355df311e50
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-12-03 21:48:28 +0530

    Create styled components for TextContainer 💻

---
+++ client/src/components/TextContainer/TextContainerElements.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 100px;
+  color: white;
+  height: 60%;
+  justify-content: space-between;
+
+  @media (min-width: 320px) and (max-width: 1200px) {
+    display: none;
+  }
+`;
+
+export const Heading = styled.h1`
+  margin-bottom: 0px;
+`;
+
+export const ActiveContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 50%;
+`;
+
+export const ActiveItem = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const ActiveIcon = styled.img`
+  padding-left: 10px;
+`;
