e97b179f22ba2c780e853a4bf000837278a9fafe
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-12-03 12:52:58 +0530

    Emit available users in a room 🌻

--- server/index.js
+++ server/index.js
@@ -35,6 +35,11 @@ io.on('connection', (socket) => {
 
     socket.join(user.room);
 
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
+
     callback();
   });
 
@@ -42,6 +47,10 @@ io.on('connection', (socket) => {
     const user = getUser(socket.id);
 
     io.to(user.room).emit('message', { user: user.name, text: message });
+    io.to(user.room).emit('roomData', {
+      room: user.room,
+      users: getUsersInRoom(user.room),
+    });
 
     callback();
   });
