619a38ee88a53bbe7f6fe5a23561cbf03d492f47
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-12-03 11:50:41 +0530

    Create Input component get socket from props. Emit message on keypress and onClick 🔥

--- client/src/components/Input/index.js
+++ client/src/components/Input/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import './input.css';
 
-const Input = () => {
-  return <div></div>;
+const Input = ({ socket }) => {
+  const sendMessage = (e) => {
+    e.preventDefault();
+    const message = e.target.value;
+    if (message) {
+      socket.emit('sendMessage', message, () => (e.target.value = ''));
+    }
+  };
+
+  return (
+    <form className="form">
+      <input
+        className="input"
+        type="text"
+        placeholder="Type a message..."
+        onKeyPress={(e) => (e.key === 'Enter' ? sendMessage(e) : null)}
+      />
+      <button className="sendButton" onClick={(e) => sendMessage(e)}>
+        Send
+      </button>
+    </form>
+  );
 };
 
 export default Input;
