24015d53fb13e75d17388c2aac2a66d81334883c
Guru Prakash Ram <69163711+guru-apr21@users.noreply.github.com>
2020-12-08 12:14:43 +0530

    Remove Link, define handleSubmit 🍈 Check for existing user in handleSubmit

--- client/src/components/Join/index.js
+++ client/src/components/Join/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import {
   JoinOuterContainer,
   JoinInnerContainer,
@@ -7,38 +7,52 @@ import {
   Input,
   Button,
 } from './JoinElements';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+toast.configure();
 const Join = () => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const history = useHistory();
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      await axios.post('http://localhost:5000/api/login', { name, room });
+      history.push(`/chat?name=${name}&room=${room}`);
+    } catch (err) {
+      toast.error(err.response.data);
+    }
+  };
 
   return (
     <JoinOuterContainer>
       <JoinInnerContainer>
         <Heading>Join Room</Heading>
-        <Input
-          placeholder="Name"
-          type="text"
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        />
-        <Input
-          $style={{ 'margin-top': '20px' }}
-          placeholder="Room"
-          type="text"
-          value={room}
-          onChange={(e) => {
-            setRoom(e.target.value);
-          }}
-        />
-        <Link
-          onClick={(e) => (!name || !room ? e.preventDefault() : null)}
-          to={`/chat?name=${name}&room=${room}`}
-        >
+        <form onSubmit={handleSubmit}>
+          <Input
+            required
+            placeholder="Name"
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+          <Input
+            required
+            $style={{ 'margin-top': '20px' }}
+            placeholder="Room"
+            type="text"
+            value={room}
+            onChange={(e) => {
+              setRoom(e.target.value);
+            }}
+          />
           <Button type="submit">Sign In</Button>
-        </Link>
+        </form>
       </JoinInnerContainer>
     </JoinOuterContainer>
   );
